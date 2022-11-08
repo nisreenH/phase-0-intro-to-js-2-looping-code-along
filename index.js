4abaf49792bbc183a10a918076fa0d1b4dc7aca1
Nisreen Hamzah <nisreenhamzah138@gmail.com>
2022-11-08 15:20:22 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+let messages = []
+function writeCards(array,event){
+    for(let i=0; i < array.length; i++){
+      messages[i]= `Thank you, ${array[i]}, for the wonderful ${event} gift!`
+    }
+    return messages
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+
+
+function countDown(x){
+    let i = x
+    while(i >= 0){
+        console.log(i)
+        i--
+    }
+}
+countDown(10)
+countDown(4)
